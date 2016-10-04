e79d12acdea9c1f1df4d55ed3e82568cf37e11ce
Anshul Sharma <optimisticanshul@gmail.com>
2016-10-04 12:10:10 +0530

    Use restart instead of start on NameNode

--- hadoop_wrapper/recipes/namenode_service.rb
+++ hadoop_wrapper/recipes/namenode_service.rb
@@ -2,28 +2,28 @@ pkg = 'hadoop-hdfs-namenode'
 service pkg do
   status_command "service #{pkg} status"
   supports [restart: true, reload: false, status: true]
-  action [:start]
+  action [:restart]
 end
 
 pkg = 'hadoop-yarn-resourcemanager'
 service pkg do
   status_command "service #{pkg} status"
   supports [restart: true, reload: false, status: true]
-  action [:start]
+  action [:restart]
 end
 
 pkg = 'hadoop-mapreduce-historyserver'
 service pkg do
   status_command "service #{pkg} status"
   supports [restart: true, reload: false, status: true]
-  action [:start]
+  action [:restart]
 end
 
 pkg = 'hadoop-hdfs-secondarynamenode'
 service pkg do
   status_command "service #{pkg} status"
   supports [restart: true, reload: false, status: true]
-  action [:start]
+  action [:restart]
 end
 
 include_recipe 'hadoop_wrapper::datanode_service'
